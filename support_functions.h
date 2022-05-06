af06e631fc5bef480153836f7c3d7d3cdca4c586
ninguemrj <53584481+ninguemrj@users.noreply.github.com>
2022-05-05 22:51:14 -0300

    Collection of functions that should be used by other libs
    under development, not used yet

---
+++ support_functions.h
@@ -0,0 +1,39 @@
+/**************************************************************************************
+ * support_funtions.cpp (v0.1)
+ * Library to be used (or not) with the PV_inverter library.
+ * Created to store read data in SD card for backup purposes.
+ * 
+ * 
+ * TODO:
+ *    - Define a standard begin for each type of message (verbose prints all)
+ *    - Create property and functions to change verbose mode on runtime
+ *    - add date and time before messages
+ *     
+ * Version info: 
+ *    - v0.1: Initial implementation for testing only
+ **************************************************************************************/
+ 
+#ifndef SUPPORT_FUNCTIONS_H
+#define SUPPORT_FUNCTIONS_H
+
+
+#ifndef STRING_H
+  #include <string.h>
+#endif
+
+
+#ifndef TIME_H
+  #include "time.h"
+#endif
+
+
+class SUPPORT_FUNCTIONS
+{
+  public:
+    void debugMsg(int _level, char _msg);    // 0 = INFO / 1 = Warning / 3 = ERROR / 4 = Debug-Verbose
+    void debugMsg(int _level, String _msg);  // 0 = INFO / 1 = Warning / 3 = ERROR / 4 = Debug-Verbose
+
+
+};
+
+#endif
