3888609b00c1f6bb0017523076eaf42912341b27
ninguemrj <53584481+ninguemrj@users.noreply.github.com>
2022-05-11 00:29:26 -0300

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
-#define VERBOSE_MODE 0    // 0 = INFO-Warning-Error   / 1 = 0 + Debug / 2 = 0 + Benchmark / 3 = 0 + Debug + Benchmark
+#define VERBOSE_MODE 1    // 0 = INFO-Warning-Error   / 1 = 0 + Debug / 2 = 0 + Benchmark / 3 = 0 + Debug + Benchmark
 #define VERBOSE_OUTPUT 0  // 0 = Serial / 1 = SD Card (yyyymmdd.log)
 #define SQL_ARRAY_SIZE 288 //  (0 to 99 = 100)
